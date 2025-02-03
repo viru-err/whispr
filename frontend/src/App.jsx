d0e0b78886475dc937bee263372d496e5dfcba86
viru-err <realvirus4u@gmail.com>
2025-02-04 00:00:52 +0530

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react'
+import { Routes,Route, Navigate } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+import SettingPage from './pages/SettingPage'
+import Profilepage from './pages/Profilepage'
+import Navbar from './components/Navbar'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const {authUser,checkAuth,isCheckingAuth}= useAuthStore()
+  useEffect(()=>{
+    checkAuth();
+  },[checkAuth]);
+  console.log("authUser");
+  if (isCheckingAuth && authUser  ){
+    return <div className="flex items-center h-screen">
+      <Loader className="size-10 animate-spin"/>
+    </div>
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={authUser?<Homepage/>:<Navigate to="/login"/>}/>
+        <Route path="/signup" element={authUser?<SignupPage/>:<Navigate to="/"></Navigate>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/settings" element={<SettingPage/>}/>
+        <Route path="/profile" element={authUser?<Profilepage/>:<Navigate to="/login"></Navigate>}/>
+      </Routes>
+      <Toaster/>
+      
+    </div>
   )
 }
 
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+import daisyui from "daisyui"
+// filepath: /c:/Users/subha/Desktop/projects/React/Whispr/frontend/tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui,],
+}
